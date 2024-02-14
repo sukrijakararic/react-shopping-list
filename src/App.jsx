562aac8e7479dfff6b2e668186fafcfd4eafbe1a
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-02-14 19:54:27 +0100

    attempted to add button functionality to list.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import React, {useState} from 'react'
-import './App.css'
-import { Header } from './components/header'
-import { ShoppingInput } from './components/ShoppingInput'
-import { ListItem } from './components/ListItem'
+import React, { useState } from "react";
+import "./App.css";
+import { Header } from "./components/header";
+import { ShoppingInput } from "./components/ShoppingInput";
+import { ListItem } from "./components/ListItem";
 
 function App() {
-
-    const [items, setItem] = useState([])
-
+  const [items, setItem] = useState([]);
 
   return (
     <>
-    <Header />
-  
-    <ShoppingInput setItem={setItem} />
-    
-    <ul>
-      {items.map((item) => {return <ListItem item={item} key={item.id}  />})}
-    </ul>
-  
+      <Header />
 
+      <ShoppingInput setItem={setItem} />
+
+      <ul>
+        {items.map((item) => {
+          return <ListItem item={item} key={item.id} />;
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ShoppingInput.jsx
+++ src/components/ShoppingInput.jsx
@@ -1,21 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-export const ShoppingInput = ({setItem}) => {
-const [text, setText] = useState('');
+export const ShoppingInput = ({ setItem }) => {
+  const [text, setText] = useState("");
 
-const handleChange = e => setText(e.target.value);
+  const handleChange = (e) => setText(e.target.value);
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  setItem((oldState) => [...oldState, {text, done: true, id: crypto.randomUUID()}]);
-  setText('');
-
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItem((oldState) => [
+      ...oldState,
+      { text, done: false, id: crypto.randomUUID() },
+    ]);
+    setText("");
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='What do you need to buy?' value={text} onChange={handleChange} />
-        <button type='submit' className='btn'>🛒</button>
+      <input
+        type="text"
+        placeholder="What do you need to buy?"
+        value={text}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn">
+        🛒
+      </button>
     </form>
-  )
-}
+  );
+};
