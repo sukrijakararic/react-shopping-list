1f253d51b951494a9a7e03e3982196e06bfc07a5
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2024-02-19 20:52:43 +0100

    added notes

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import { ShoppingInput } from "./components/ShoppingInput";
 import { ListItem } from "./components/ListItem";
 
 function App() {
-  const [items, setItem] = useState([]);
+  const [items, setItem] = useState([]); // items has been initialized as an empty array
 
   return (
     <>
       <Header />
-
+      
+      {/* we pass set item to the Shopping input so we can use it to set the state of the items array */}
       <ShoppingInput setItem={setItem} />
 
       <ul>
+
+        {/*The "items" here represents the array that holds each individual "item" so mapping the item from the items array is taking each item and displaying it*/}
         {items.map((item) => {
+          {/* we pass the individaul items to the ListItem component to be used in that respective component */}
           return <ListItem item={item} key={item.id} />;
         })}
       </ul>
--- src/components/ShoppingInput.jsx
+++ src/components/ShoppingInput.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 export const ShoppingInput = ({ setItem }) => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState(""); // we use the state hook here to catch the change of typing into the input. When i type into the input below, it will change the text state
 
-  const handleChange = (e) => setText(e.target.value);
+  const handleChange = (e) => setText(e.target.value); // This function will change the state of text of whatever is written
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,6 +16,8 @@ export const ShoppingInput = ({ setItem }) => {
 
   return (
     <form onSubmit={handleSubmit}>
+      
+        {/*The value of the input is text so that its tied together and the onChange just calls the handleChange function. onChange fires everytime the input changes */}
       <input
         type="text"
         placeholder="What do you need to buy?"
